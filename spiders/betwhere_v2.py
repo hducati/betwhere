3f95b11b037a6b309e2b26a7d6d9a10f92a80a70
unknown <henrique.dcti@gmail.com>
2020-01-10 16:36:45 -0300

    New functions

--- spiders/betwhere_v2.py
+++ spiders/betwhere_v2.py
@@ -15,12 +15,15 @@ file_name = 'matches/txt/matchs_{}_{}.txt'.format(today_date, tomorrow_formatted
 match_file = open(join(getcwd(), file_name), 'w')
 
 URL_EXTRACT = ['https://www.fctables.com/todays-match-predictions/' + tomorrow_formatted]
+
 last_index = 1
 overall_index = 1
 all_games = 1
 
 df = DataFrame(columns=('Data jogo', 'Jogo', 'Times', 'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS'))
-df_overall = DataFrame(columns=('Data jogo', 'Jogo', 'Times','Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS'))
+df_overall = DataFrame(columns=(
+    'Data jogo', 'Jogo', 'Times','Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 3.5', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS',
+    'Escanteios p/ jogo', 'Média cartão amarelo', 'Total cartão amarelo', 'Média cartão vermelho', 'Total cartão vermelho'))
 
 
 class BetwhereSpider(scrapy.Spider):
@@ -51,9 +54,6 @@ class BetwhereSpider(scrapy.Spider):
     def match_request(self, response):
         global today_date
         global tomorrow_formatted
-        global df
-        global df_overall
-        global all_games
 
         self.logger.info('Extracting data from {}'.format(response.url))
 
@@ -74,17 +74,62 @@ class BetwhereSpider(scrapy.Spider):
         status = response.css("div.round-cd span::text").extract_first()
         
         last_stats = response.css("div.team_stats_forms ul li div::text").extract()
-
         overall_stats = response.css("div.team_stats_item ul li div::text").extract()
 
-        self.write_txt_file(home_team, away_team, cup_name, status, last_stats, overall_stats)
-        self.write_excel_file(request_date, home_team, away_team, last_stats, overall_stats)
+        home_match_dict = {}
+        away_match_dict = {}
 
+        self.write_txt_file(home_team, away_team, cup_name, status, last_stats, overall_stats)
+
+        if response.css("#team_stats_vs > tbody > tr:nth-child(15) > td:nth-child(2)::text").extract_first() == 'Corners per game':
+            corners_per_game_home = response.css("#team_stats_vs > tbody > tr:nth-child(15) > td:nth-child(1)::text").extract_first()
+            corners_per_game_away = response.css("#team_stats_vs > tbody > tr:nth-child(15) > td:nth-child(3)::text").extract_first()
+        else:
+            corners_per_game_away = ''
+            corners_per_game_home = ''
+
+        if response.css("#team_stats_vs > tbody > tr:nth-child(32) > td:nth-child(2)::text").extract_first() == 'Yellow cards avg':
+            yellowcards_home = response.css("#team_stats_vs > tbody > tr:nth-child(32) > td:nth-child(1)::text").extract_first()
+            yellowcards_away = response.css("#team_stats_vs > tbody > tr:nth-child(32) > td:nth-child(3)::text").extract_first()
+
+            yellowcards_home_formatted = yellowcards_home.replace('(','').replace(')', '').split(' ')
+            yellowcards_away_formatted = yellowcards_away.replace('(','').replace(')', '').split(' ')
+        else:
+            yellowcards_home_formatted = ['', '']
+            yellowcards_away_formatted = ['', '']
+
+        if response.css("#team_stats_vs > tbody > tr:nth-child(33) > td:nth-child(2)::text").extract_first() == 'Red cards avg':
+            redcards_home = response.css("#team_stats_vs > tbody > tr:nth-child(33) > td:nth-child(1)::text").extract_first()
+            redcards_away = response.css("#team_stats_vs > tbody > tr:nth-child(33) > td:nth-child(3)::text").extract_first()
+
+            redcards_home_formatted = redcards_home.replace('(','').replace(')', '').split(' ')
+            redcards_away_formatted = redcards_away.replace('(','').replace(')', '').split(' ')
+        else:
+            redcards_home_formatted = ['', '']
+            redcards_away_formatted = ['', '']
+
+        if response.css("#team_stats_vs > tbody > tr:nth-child(11) > td:nth-child(2)::text").extract_first() == 'Over 3.5':
+            over_tres_cinco_home = response.css("#team_stats_vs > tbody > tr:nth-child(11) > td:nth-child(1)::text").extract_first()
+            over_tres_cinco_away = response.css("#team_stats_vs > tbody > tr:nth-child(11) > td:nth-child(3)::text").extract_first()
+        else:
+            over_tres_cinco_home = ''
+            over_tres_cinco_away = ''
+
+        home_match_dict = {'Corners': corners_per_game_home, 'Yellow cards avg': yellowcards_home_formatted[0], 
+        'Yellow cards total': yellowcards_home_formatted[1],'Red cards avg': redcards_home_formatted[0], 
+        'Red cards total': redcards_home_formatted[1], 'Over 3.5': over_tres_cinco_home}
+
+        away_match_dict = {'Corners': corners_per_game_away, 'Yellow cards avg': yellowcards_away_formatted[0], 
+        'Yellow cards total': yellowcards_away_formatted[1], 'Red cards avg': redcards_away_formatted[0], 
+        'Red cards total': redcards_away_formatted[1], 'Over 3.5': over_tres_cinco_away}
+        
+        self.write_excel_file(request_date, home_team, away_team, last_stats, overall_stats, home_match_dict, away_match_dict)
+    
     def write_txt_file(self, home_team, away_team, cup_name, status, last_stats, overall_stats):
         
         stats_overview = [
             'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS',
-            'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS'
+            'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS', 
         ]
 
         match_file.write(' | ' + cup_name + ' | '  + home_team + '   X   '  + away_team + '(' + status + ')' + '\n\n')
@@ -106,7 +151,7 @@ class BetwhereSpider(scrapy.Spider):
 
         match_file.write('\n')
 
-    def write_excel_file(self, match_date, home_team, away_team, last_stats, overall_stats):
+    def write_excel_file(self, match_date, home_team, away_team, last_stats, overall_stats, home_dict, away_dict):
         global last_index
         global overall_index
         global df
@@ -119,8 +164,10 @@ class BetwhereSpider(scrapy.Spider):
         ]
 
         overall_stats_overview = [
-            'Data jogo', 'Jogo', 'Times', 'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS',
-            'Data jogo', 'Jogo', 'Times', 'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS'
+            'Data jogo', 'Jogo', 'Times', 'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 3.5', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS',
+            'Escanteios p/ jogo', 'Média cartão amarelo', 'Total cartão amarelo', 'Média cartão vermelho', 'Total cartão vermelho',
+            'Data jogo', 'Jogo', 'Times', 'Partidas', 'Gols', 'Por jogo', 'Vitórias', 'Empates', 'Derrotas', 'Over 3.5', 'Over 2.5', 'Over 1.5', 'CS', 'BTTS',
+            'Escanteios p/ jogo', 'Média cartão amarelo', 'Total cartão amarelo', 'Média cartão vermelho', 'Total cartão vermelho'
         ]
 
         value_support = 0
@@ -132,7 +179,7 @@ class BetwhereSpider(scrapy.Spider):
         last_stats.insert(12, 'Jogo ' + str(all_games))
 
         last_stats.insert(0, str(match_date))
-        last_stats.insert(13, '')
+        last_stats.insert(13, str(match_date))
 
         overall_stats.insert(0, home_team)
         overall_stats.insert(11, away_team)
@@ -141,7 +188,25 @@ class BetwhereSpider(scrapy.Spider):
         overall_stats.insert(12, 'Jogo ' + str(all_games))
 
         overall_stats.insert(0, str(match_date))
-        overall_stats.insert(13, '')
+        overall_stats.insert(13, str(match_date))
+
+        overall_stats.insert(9, home_dict['Over 3.5'])
+        overall_stats.insert(23, away_dict['Over 3.5'])
+
+        overall_stats.insert(14, home_dict['Corners'])
+        overall_stats.insert(len(overall_stats), away_dict['Corners'])
+
+        overall_stats.insert(15, home_dict['Yellow cards avg'])
+        overall_stats.insert(len(overall_stats), away_dict['Yellow cards avg'])
+
+        overall_stats.insert(16, home_dict['Yellow cards total'])
+        overall_stats.insert(len(overall_stats), away_dict['Yellow cards total'])
+
+        overall_stats.insert(17, home_dict['Red cards avg'])
+        overall_stats.insert(len(overall_stats), away_dict['Red cards avg'])
+
+        overall_stats.insert(18, home_dict['Red cards total'])
+        overall_stats.insert(len(overall_stats), away_dict['Red cards total'])
 
         for ls, ss in zip(last_stats, stats_overview):
 
@@ -154,7 +219,7 @@ class BetwhereSpider(scrapy.Spider):
         value_support = 0
       
         for overall, overview in zip(overall_stats, overall_stats_overview):
-            if value_support == 13:
+            if value_support == 19:
                 overall_index += 1
 
             df_overall.set_value(index=overall_index, col=overview, value=overall)
@@ -174,10 +239,6 @@ class BetwhereSpider(scrapy.Spider):
         writer = ExcelWriter(excel_path)
         
         self.logger.info('Started writing in excel file...')
-
-        # df.sort_values('Data jogo', ascending=True,  inplace=True)
-
-        # df_overall.sort_values('Data jogo', ascending=True, inplace=True)
 
         df.to_excel(writer, "Últimos 6 jogos", index=True, header=True)
         df_overall.to_excel(writer, "Todos os jogos", index=True, header=True)
