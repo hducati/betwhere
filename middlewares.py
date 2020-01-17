8d263f412084ebec072ecebfa4a25497d69df492
unknown <henrique.dcti@gmail.com>
2020-01-16 23:56:39 -0300

    Add HTTP middleware

--- middlewares.py
+++ middlewares.py
@@ -101,3 +101,17 @@ class AutomationBetwhereDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+import base64
+class ProxyMiddleware(object):
+    # overwrite process request
+    def process_request(self, request, spider):
+        # Set the location of the proxy
+        request.meta['proxy'] = "http://200.33.152.207:45226"
+
+        # Use the following lines if your proxy requires authentication
+        # proxy_user_pass = "USERNAME:PASSWORD"
+        # setup basic authentication for the proxy
+        # encoded_user_pass = base64.encodestring(proxy_user_pass)
+        # request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
